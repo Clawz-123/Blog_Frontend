9ad6fa43ab493a361b405066f62aec41ecb74a52
Clawz-123 <pukarbohara9@gmail.com>
2025-07-23 15:13:38 +0545

    making branches

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+      <Router>
+        <Routes>
+          <Route path=''/>
+        </Routes>
+      </Router>
       
     </div>
   )

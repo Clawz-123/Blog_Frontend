c60c9674824825fc419746281d8bb5ba644638a9
Clawz-123 <pukarbohara9@gmail.com>
2025-07-24 14:41:30 +0545

    creating array for signup form

--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
+const SignupField = [
+  { label: 'First Name', name: 'firstName', type: 'text', placeholder: 'Enter your first name' },
+  { label: 'Last Name', name: 'lastName', type: 'text', placeholder: 'Enter your last name' },
+  { label: 'Email Address', name: 'email', type: 'email', placeholder: 'Enter your email' },
+  { label: 'Password', name: 'password', type: 'password', placeholder: 'Create a password' },
+  { label: 'Confirm Password', name: 'confirmPassword', type: 'password', placeholder: 'Re-enter your password' }
+];
+
+
 const SignUp = () => {
   return (
     <div>
